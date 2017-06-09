4b4f94d64ed1d4c2c511f4a240411a18a94f263f
kongxx3282 <kongxx3282@gmail.com>
2017-06-09 15:55:39 +0800

    Add files via upload

--- Catalog_Manager.cpp
+++ Catalog_Manager.cpp
@@ -16,7 +16,7 @@ void Create_Database(CString DB_Name)
 		if (flag) {
 			if (CreateDirectory(fpath + "\\catalog", NULL) && CreateDirectory(fpath + "\\table", NULL) && CreateDirectory(fpath + "\\index", NULL)) 
 			{
-				if (CreateDirectory(fpath + "\\catalog\\table_num", NULL) && CreateDirectory(fpath + "\\catalog\\table_attr", NULL) && CreateDirectory(fpath + "\\catalog\\table_info", NULL)) 
+				if (CreateDirectory(fpath + "\\catalog\\table_num", NULL) && CreateDirectory(fpath + "\\catalog\\table_attr", NULL) && CreateDirectory(fpath + "\\catalog\\table_info", NULL) && CreateDirectory(fpath + "\\catalog\\index_info", NULL))
 				{
 					ofstream table_num;//创建table_num文件
 					table_num.open(fpath + "\\catalog\\table_num\\table_num.txt");
@@ -81,6 +81,7 @@ void Create_Table(CString Table_Name, CString Attr, CString DB_Name, CString & A
 			{
 				count++;
 				temp = Attr.Mid(start, end - start);
+				temp += " NULL";
 				fpath.Write((unsigned char *)(temp.GetBuffer()),temp.GetLength());//存到table_info表中
 				fpath.Write(line.GetBuffer(), line.GetLength());
 				if (temp.Find('#') != -1)//有主键定义
@@ -158,7 +159,9 @@ void Create_Table(CString Table_Name, CString Attr, CString DB_Name, CString & A
 //创建索引
 //先查找表名对不对
 //然后看属性名对不对
+//是否已经有索引
 //然后看是不是unique
+//修改table_info文件的索引名
 void Create_Index(CString Index_Name, CString Table_Name, CString Attr_Name, CString DB_Name, int & length, int & offset, int & type)
 {
 	CStdioFile table_file;
@@ -174,7 +177,7 @@ void Create_Index(CString Index_Name, CString Table_Name, CString Attr_Name, CSt
 				CString strLine(_T(""));
 				CString temp_attr;
 				int index;
-				int flag = 0;
+				int flag = 0,line_offset = 0;
 				while(table_file.ReadString(strLine))
 				{
 					index = strLine.Find(' ');
@@ -184,39 +187,83 @@ void Create_Index(CString Index_Name, CString Table_Name, CString Attr_Name, CSt
 						flag = 1;//找到了对应属性
 						break;
 					}
+					line_offset += strLine.GetLength();
 					strLine = _T("");
 				}
 				if (flag)//属性名存在
 				{
-					CString whether_unique;
-					whether_unique = strLine.Right(1);//嗨呀好像有点不对……如果一个属性只能建一次索引的话，还要考虑一下索引是否存在，那这样我的table_info格式就要改了癌癌癌明天起床再说
-					if (whether_unique == '1')//属性为unique
+					CString whether_exist_index;
+					int last_end;
+					last_end = strLine.ReverseFind(' ');
+					whether_exist_index = strLine.Mid(last_end + 1, strLine.GetLength() - last_end - 1);//最后一个空格后的词
+					if (whether_exist_index == "NULL")//索引不存在
 					{
-						CString attr_type;
-						int type_end;
-						type_end = strLine.Find(' ', index);
-						attr_type = strLine.Mid(index + 1, type_end - index - 1);//获取表示属性类型的符号 +:int -:float 数字:char
-						if (attr_type == '+')//int
+						CString whether_unique;
+						strLine = strLine.Left(last_end);
+						whether_unique = strLine.Right(1);
+						if (whether_unique == '1')//属性unique,折腾到这终于能建了
 						{
-							type = 0;
-							length = 5;
-						}
-						else if (attr_type == '-')//float
-						{
-							type = 1;
-							length = 10;
-						}
-						else//char
-						{
-							type = 2;
-							length = _ttoi(attr_type);
-						}
+							CString attr_type;
+							int type_end;
+							type_end = strLine.Find(' ', index);
+							attr_type = strLine.Mid(index + 1, type_end - index - 1);//获取表示属性类型的符号 +:int -:float 数字:char
+							if (attr_type == '+')//int
+							{
+								type = 0;
+								length = 5;
+							}
+							else if (attr_type == '-')//float
+							{
+								type = 1;
+								length = 10;
+							}
+							else//char
+							{
+								type = 2;
+								length = _ttoi(attr_type);
+							}
 
-						//还要添加相应的catalog文件信息
+							//还要添加相应的catalog文件信息
+							//先改table_info,那一行后边把NULL改成索引名就行了……
+							CString NewLines[33];//最多32个属性+一个主键定义
+							CString line = "\r\n";
+							NewLines[0] = strLine+' ' + Index_Name;//第一行是要改的那个
+							//这接着先把后边的东西也全读出来
+							int i = 1;
+							while (table_file.ReadString(strLine))
+							{
+								NewLines[i++] = strLine;
+							}
+
+							
+							i = 0;//再全写回去
+							table_file.Seek(line_offset, CFile::begin);
+							while (!NewLines[i].IsEmpty()) 
+							{
+								table_file.Write(NewLines[i].GetBuffer(), NewLines[i].GetLength());//不能这么写……这样会把原来的给覆盖掉……要么就把它之后的东西也都读出来，然后接着写回去
+								table_file.Write(line.GetBuffer(), line.GetLength());//换行
+								i++;
+							}
+
+							//然后再更新index_info的信息
+							CString index_info = "..\\" + DB_Name + "\\catalog\\index_info\\" + Index_Name + ".txt";
+							CFile index_info_file;
+							index_info_file.Open(index_info, CFile::modeCreate | CFile::modeNoTruncate | CFile::modeWrite);
+							CString temp = Table_Name+' '+Attr_Name;
+							//index_info_file.SeekToEnd();
+							index_info_file.Write(temp.GetBuffer(), temp.GetLength());
+							index_info_file.Close();
+
+							cout << "Create index successfully!" << endl;
+						}
+						else//属性非unique
+						{
+							cout << "Can't create index on a non-unique attribute!" << endl;
+						}
 					}
-					else//属性非unique
+					else//该属性的索引已经存在
 					{
-						cout << "Can not create index on this attribute!" << endl;
+						cout << "There is already an index on this attribute!" << endl;
 					}
 				}
 				else//属性名错误
@@ -299,19 +346,162 @@ void Drop_Database(CString DB_Name)
 		cout << "No such a database!" << endl;
 	}
 }
-/*
+
 //删除表
+//先找有没有这个表
+//然后把这个表上所有的索引删掉
+//然后修改table_info table_attr table_num吧……？
 void Drop_Table(CString Table_Name, CString DB_Name, CString index_name[32], int & count)
 {
+	CString table_info_path = "..\\" + DB_Name + "\\catalog\\table_info\\" + Table_Name + ".txt";
+	CStdioFile table_info;
+	try
+	{
+		if (table_info.Open(table_info_path, CFile::modeRead))//有这个表
+		{
+			CString strLine,temp;
+			int index_end;//i是index_name数组的下标
+			count = 0;
+			CString index_info_path;
+			CFile index_info_file;
+			while (table_info.ReadString(strLine))
+			{
+				index_end = strLine.ReverseFind(' ');
+				temp = strLine.Right(strLine.GetLength() - index_end-1);
+				if (temp != "NULL")//该属性存在索引，去index_info文件夹里把对应文件删掉
+				{
+					index_name[count++] = temp;
+					index_info_path = "..\\" + DB_Name + "\\catalog\\index_info\\" + temp + ".txt";
+					index_info_file.Remove(index_info_path);
 
+				}
+				
+			}
+			table_info.Close();
+			table_info.Remove(table_info_path);//删掉table_info里的文件
+
+			//然后就是改table_attr和table_num
+			//table_num比较容易，先来吧……
+			CString table_num_path = "..\\" + DB_Name + "\\catalog\\table_num\\table_num.txt";
+			CStdioFile table_num_file;
+			table_num_file.Open(table_num_path, CFile::modeReadWrite);
+
+			int table_num;
+			if (table_num_file.ReadString(strLine))
+			{
+				table_num = _ttoi(strLine);
+				table_num--;
+			}
+			CString strNewLine;
+			strNewLine.Format("%d", table_num);
+			table_num_file.SetLength(0);
+			table_num_file.WriteString(strNewLine);
+			table_num_file.Close();
+			
+			//突然开始思考table_attr这个信息有用吗…………陷入沉思
+			//先放着，以后用到再回来填这个坑
+
+			wcout << "Drop table " << Table_Name.GetString() << " successfully!" << endl;
+		}
+		else//没有这个表
+		{
+			wcout << "No such a table named " <<Table_Name.GetString() << endl;
+		}
+	}
+	catch (CException* e)
+	{
+		e->ReportError();
+		e->Delete();
+	}
 }
 
 //删除索引
+//先看索引是否存在
+//然后到index_info文件夹里找到它在哪张表上的哪个属性上，顺便删除index_info文件
+//然后到table_info文件夹的对应文件里更新一下
 void Drop_Index(CString Index_Name, CString DB_Name)
 {
+	CString index_info_path = "..\\" + DB_Name + "\\catalog\\index_info\\" + Index_Name + ".txt";
+	CStdioFile index_info;
+	try {
+		if (index_info.Open(index_info_path, CFile::modeRead))//有这个索引字典文件
+		{
+			CString temp;
+			CString table_name, attr_name;
+			int end;
+			index_info.ReadString(temp);
+			end = temp.Find(' ');
+			table_name = temp.Left(end);
+			attr_name = temp.Right(temp.GetLength() - end - 1);//获取表名和属性名
+			index_info.Close();
+
+			//查找有无表名
+			CString file_name = "..\\" + DB_Name + "\\catalog\\table_info\\" + table_name + ".txt";
+			CStdioFile table_file;
+			if (table_file.Open(file_name, CFile::modeReadWrite))//存在该表
+			{
+				CString strLine, temp_name;
+				int attr_name_index;
+				int line_offset = 0;//记录偏移量
+				while (table_file.ReadString(strLine))
+				{
+					attr_name_index = strLine.Find(' ');
+					temp_name = strLine.Left(attr_name_index);
+
+					if (temp_name == attr_name)//找到对应的属性名
+					{
+						break;
+					}
+					line_offset += strLine.GetLength();
+				}
+
+				end = strLine.ReverseFind(' ');
+				strLine = strLine.Left(end);
+				//修改该属性对应的那行的索引信息
+				CString NewLines[33];//最多32个属性+一个主键定义
+				CString line = "\r\n";
+				NewLines[0] = strLine + ' ' + "NULL";//第一行是要改的那个
+														 //这接着先把后边的东西也全读出来
+				int i = 1;
+				while (table_file.ReadString(strLine))
+				{
+					NewLines[i++] = strLine;
+				}
+
+				i = 0;//再全写回去
+				table_file.Seek(line_offset, CFile::begin);
+				while (!NewLines[i].IsEmpty())
+				{
+					table_file.Write(NewLines[i].GetBuffer(), NewLines[i].GetLength());
+					table_file.Write(line.GetBuffer(), line.GetLength());//换行
+					i++;
+				}
+
+				cout << "Drop index successfully!" << endl;
+				table_file.Close();
+				index_info.Remove(index_info_path);//删除索引成功之后删除字典文件
+			}
+			else
+			{
+
+				cout << "something wrong!" << endl;//应该是不会发生这个的……
+			}
+
+
+		}
+		else
+		{
+			cout << "No such an index!" << endl;
+		}
+	}
+	catch (CException* e)
+	{
+		e->ReportError();
+		e->Delete();
+	}
 
 }
-
+/*
 //验证并增加属性值
 bool verify_add_attr(CString & attr, CString & temp, int type, int length)
 {
@@ -335,25 +525,213 @@ void Empty_Table(CString DB_Name, CString Table_Name, CString index_name[32], in
 {
 
 }
+*/
+void Get_each_condition(CString one_cond, condition_info conds[32], int& count)
+{
+	CString temp_attr, op, right_type, right_value;
+	int op_start, op_end;
 
-//转换条件组delete
+	op_end = one_cond.ReverseFind(' ');
+	op_start = op_end - 2;
+	op = one_cond.Mid(op_start, 2);
+	temp_attr = one_cond.Left(op_start);//提取属性名
+
+	right_type = one_cond.GetAt(op_end + 1);//获取右侧值的数据类型
+	right_value = one_cond.Right(one_cond.GetLength() - op_end - 2);//获取右侧值
+	//wcout << temp_attr.GetString() << " " << op.GetString() << " " << right_type.GetString() << " " << right_value.GetString() << endl;//测试提取是否成功用
+	conds[count].left_attr = temp_attr;//先把这些信息都存起来，对不对再说……
+	conds[count].condition = op;
+	conds[count].const_data = right_value;
+	conds[count].right_type = right_type;
+	count++;
+}
+//转换where条件组
+//第一件事还是看表名对不对
+//然后是不是要验证那些属性是不是这个表里的
+//都对的话应该就可以存起来了吧
+//想必还要存一下索引信息留给后边用....?不知道……
+//1表示验证成功，0表示验证失败？
 bool Get_Condition(CString DB_Name, CString Table_Name, CString Condition, condition_info conds[32], int & count, char & cond, index_info nodes[32], int & num)
 {
+	CString table_info_path = "..\\" + DB_Name + "\\catalog\\table_info\\" + Table_Name + ".txt";
+	CStdioFile table_info;
+	try
+	{
+		if (table_info.Open(table_info_path, CFile::modeRead))//有这张表
+		{
+			table_info.Close();
+			//以&和.为界分隔Condition
+	        int start = 0,end;
+			CString one_cond;
+			/*
+			CString temp_attr,op,right_type,right_value;
+			int op_start,op_end;*/
+			count = 0;
+			while ((end = Condition.Find('&', start)) != -1)//找得到&
+			{
+				one_cond = Condition.Mid(start, end - start);//获取单独的一个condition
+				start = end + 1;
+				Get_each_condition(one_cond, conds, count);
+				/*
+				op_end = one_cond.ReverseFind(' ');
+				op_start = op_end - 2;
+				op = one_cond.Mid(op_start, 2);
+				//结构体里的Condition也是运算符的意思……？那传入参数的那个cond是啥呢呢呢呢呢
+				//先把它们都存成运算符好了
+				//不对，要先判断属性名对不对
+				//癌，先把所有信息都提取出来，然后再想下一步怎么办吧
+				temp_attr = one_cond.Left(op_start);//提取属性名
 
+				right_type = one_cond.GetAt(op_end+1);//获取右侧值的数据类型
+				right_value = one_cond.Right(one_cond.GetLength() - op_end-2);//获取右侧值
+				//wcout << temp_attr.GetString() << " " << op.GetString() << " " << right_type.GetString() << " " << right_value.GetString() << endl;//测试提取是否成功用
+				conds[count].left_attr = temp_attr;//先把这些信息都存起来，对不对再说……
+				conds[count].condition = op;
+				conds[count].const_data = right_value;
+				conds[count].right_type = right_type;
+				count++;
+				*/
+			}
+			//然后要获取最后一个属性的信息
+			end = Condition.GetLength() - 1;//要忽略掉最后那个. 干嘛要在最后加个点呢，啧……
+
+			one_cond = Condition.Mid(start, end - start);//获取单独的一个condition
+			start = end + 1;
+			Get_each_condition(one_cond, conds, count);
+		  
+			//然后应该验证1.左侧属性是否存在 2.左右属性类型是否一致
+			attr_info attrs[32];
+			int attr_count;
+			Get_Attr_Info_All(DB_Name, Table_Name, attrs, attr_count);//把table_info里的信息全都获取出来
+			
+			int i,j;
+			int flag;
+			CString index_name;
+			for (i = 0; i < count; i++)//遍历conds,逐个验证……
+			{
+				flag = 0;
+				for (j = 0; j < attr_count; j++)
+				{
+					//wcout << "conds[i]: " << conds[i].left_attr.GetString() << conds[i].right_type << endl;
+					//wcout << "attrs[j]: " << attrs[j].attr_name.GetString() << attrs[j].type << endl;
+					if (conds[i].left_attr == attrs[j].attr_name)//找到了对应的属性
+					{
+						conds[i].left_type = attrs[j].type;
+						flag = 1;
+						if (conds[i].left_type == conds[i].right_type)
+						{
+							//验证成功了
+							//接下来是不是应该完善一下conds,看看那些属性有没有索引什么的
+							//先空着
+							//cout << "验证成功了" << endl;
+							flag = 2;
+							break;
+
+						}
+						else
+						{
+							break;
+						}
+			
+					}
+					
+				}
+				if (flag == 0)
+				{
+					wcout << "There is no such an attribute named " << conds[i].left_attr.GetString() << " in the table!" << endl;
+					break;
+				}
+				else if (flag == 1)
+				{
+					wcout << "The type of " << conds[i].const_data.GetString() << " is wrong!" << endl;
+					break;
+				}
+
+			}
+
+			//获取索引信息?
+
+
+			return true;
+		}
+		else//没有这个表
+		{
+			wcout << "No such a table named " << Table_Name.GetString() << endl;
+			return false;
+		}
+		}
+	catch (CException* e)
+	{
+		e->ReportError();
+		e->Delete();
+	}
 }
-
+/*
 //获取显示记录格式
 void Get_Attr_Info(CString DB_Name, CString Table_Name, attr_info print[32], int & count, CString Attr)
 {
 
 }
-
+*/
 //获取一张表的所有属性的信息
+//先看有没有这张表
+//有的话就开始遍历~
 void Get_Attr_Info_All(CString DB_Name, CString Table_Name, attr_info print[32], int & count)
 {
+	CString table_info_path = "..\\" + DB_Name + "\\catalog\\table_info\\" + Table_Name + ".txt";
+	CStdioFile table_info;
+	try
+	{
+		if (table_info.Open(table_info_path, CFile::modeRead))//有这张表
+		{
+			CString strLine;
+			CString temp_type;
+			int attr_end,type_end;
+			int length = 0;
+			count = 0;
+			while (table_info.ReadString(strLine))
+			{
+				if (strLine.Find('#') == -1)//主键定义的那行不算
+				{
+					attr_end = strLine.Find(' ');
+					print[count].attr_name = strLine.Left(attr_end);
+					type_end = strLine.Find(' ',attr_end+1);
+					temp_type = strLine.Mid(attr_end + 1, type_end - attr_end-1);
+
+					if (temp_type == '+')//属性类型 int:+ float:- char:/
+					{
+						print[count].type = '+';
+					}
+					else if (temp_type == '-')
+					{
+						print[count].type = '-';
+					}
+					else
+					{
+						print[count].type = '/';
+					}
+
+					print[count].offset = length;//这样offset是这行记录在table_info文件中的位置，length是它这一行的长度
+					print[count].length = strLine.GetLength();
+					length += strLine.GetLength();//不知道这样对不对，暂时先这样放着
+
+					count++;
+				}
+			}
+		}
+		else//没有这个表
+		{
+			wcout << "No such a table named " << Table_Name.GetString() << endl;
+		}
+	}
+	catch (CException* e)
+	{
+		e->ReportError();
+		e->Delete();
+	}
 
 }
-
+/*
 //获取表中所有的索引信息
 void Get_All_Index(CFile & file, long header, int attrs, index_info nodes[32], int & num)
 {
